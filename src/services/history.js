9f3310d0638074e2a68c1f2a24c0947905412c06
mrmauricio <mendes-mauricio@outlook.com>
2019-08-08 15:42:38 +0100

    Created Page and Route Structure

---
+++ src/services/history.js
@@ -0,0 +1,7 @@
+// history é uma lib que possibilita navegar o usuário para rotas de todos
+// os lugares, incluindo do próprio redux
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+export default history;
