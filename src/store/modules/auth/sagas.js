c41ca6db1a446a46adaa2dbc5aa4e0c716a8baca
mrmauricio <mendes-mauricio@outlook.com>
2019-08-12 17:12:52 +0100

    Loading Auth

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,33 +3,37 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 // funções são exportadas para facilitar os testes
 export function* signIn({ payload }) {
-    const { email, password } = payload;
+    try {
+        const { email, password } = payload;
 
-    // call pois está executando um método que retorna promise, assíncrono
-    // primeiro parâmetro é a url que queremos chamar (sessions), e o segundo
-    // são os parametros passados
-    const response = yield call(api.post, 'sessions', {
-        email,
-        password,
-    });
+        // call pois está executando um método que retorna promise, assíncrono
+        // primeiro parâmetro é a url que queremos chamar (sessions), e o segundo
+        // são os parametros passados
+        const response = yield call(api.post, 'sessions', {
+            email,
+            password,
+        });
 
-    // essa rota retornará o user e o token, caso dados passados batam
-    const { token, user } = response.data;
+        // essa rota retornará o user e o token, caso dados passados batam
+        const { token, user } = response.data;
 
-    if (!user.provider) {
-        console.tron.error('usuário nao é prestador');
-        return;
+        if (!user.provider) {
+            console.tron.error('usuário nao é prestador');
+            return;
+        }
+
+        // chama a action de sucesso passando o token e o usuário
+        yield put(signInSuccess(token, user));
+
+        // redireciona o usuário logado para a dashboard
+        history.push('/dashboard');
+    } catch (err) {
+        yield put(signFailure());
     }
-
-    // chama a action de sucesso passando o token e o usuário
-    yield put(signInSuccess(token, user));
-
-    // redireciona o usuário logado para a dashboard
-    history.push('/dashboard');
 }
 
 // sempre que houver o dispatch da action em questão, o takeLatest pega a
