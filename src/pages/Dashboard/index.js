05aa156a1b6752c4085d5007d6b83b7cf9140998
mrmauricio <mendes-mauricio@outlook.com>
2019-08-23 16:10:48 +0100

    Listing Schedule

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,75 @@
-import React, { useState, useMemo } from 'react';
-import { format, subDays, addDays } from 'date-fns';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+    format,
+    subDays,
+    addDays,
+    setHours,
+    setMinutes,
+    setSeconds,
+    isBefore,
+    isEqual,
+    parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+// isso deveria ser trazido do back-end, é a range de horários que podem
+// ser feitos os agendamentos
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+    const [schedule, setSchedule] = useState([]);
     const [date, setDate] = useState(new Date());
 
     const dateFormatted = useMemo(
         () => format(date, "d 'de' MMMM", { locale: pt }),
         [date]
     );
+
+    useEffect(() => {
+        async function loadSchedule() {
+            const response = await api.get('schedule', {
+                // query params
+                params: { date },
+            });
+
+            // pega a timezone do usuário
+            const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+            console.log(timezone);
+
+            const data = range.map(hour => {
+                // transforma a date atual em um horário cheio, para então
+                // comparar com as datas dos agendamentos (back-end só per-
+                // mite agendar em hora cheia)
+                const checkDate = setSeconds(
+                    setMinutes(setHours(date, hour), 0),
+                    0
+                );
+                // transforma de UTC (horario global) para a timezone
+                const compareDate = utcToZonedTime(checkDate, timezone);
+
+                // informações que serão mostradas na tela: time = horarios,
+                // past = caso ja tenha passado p/ aplicar o css, appoint =
+                // verifica na API se teve algum agendamento pro horario
+                return {
+                    time: `${hour}:00h`,
+                    past: isBefore(compareDate, new Date()),
+                    appointment: response.data.find(a =>
+                        isEqual(parseISO(a.date), compareDate)
+                    ),
+                };
+            });
+
+            // seta a array com os dados de agendamento
+            setSchedule(data);
+        }
+
+        loadSchedule();
+    }, [date]);
 
     function handlePrevDay() {
         setDate(subDays(date, 1));
@@ -35,22 +92,20 @@ export default function Dashboard() {
             </header>
 
             <ul>
-                <Time past>
-                    <strong>08:00</strong>
-                    <span>Diego Fernandes</span>
-                </Time>
-                <Time available>
-                    <strong>09:00</strong>
-                    <span>Em aberto</span>
-                </Time>
-                <Time>
-                    <strong>10:00</strong>
-                    <span>Diego Fernandes</span>
-                </Time>
-                <Time>
-                    <strong>11:00</strong>
-                    <span>Em aberto</span>
-                </Time>
+                {schedule.map(time => (
+                    <Time
+                        key={time.time}
+                        past={time.past}
+                        available={!time.appointment}
+                    >
+                        <strong>{time.time}</strong>
+                        <span>
+                            {time.appointment
+                                ? time.appointment.user.name
+                                : 'Em Aberto'}
+                        </span>
+                    </Time>
+                ))}
             </ul>
         </Container>
     );
